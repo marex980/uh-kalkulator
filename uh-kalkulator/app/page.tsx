ba2f739892c24c050c27f3f2aaa0bfaaf78c39c5
marex980 <marex9801@gmail.com>
2024-09-10 00:37:00 +0200

    upload code

---
+++ uh-kalkulator/app/page.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ingredientsList, { Ingredient } from "../components/IngredientsList"; // Import the list
+
+const Home: React.FC = () => {
+  const [hasMounted, setHasMounted] = useState(false);
+  const [selectedIngredients, setSelectedIngredients] = useState<Ingredient[]>(
+    []
+  );
+  const [totalCarbs, setTotalCarbs] = useState<number>(0);
+  const [selectedIngredient, setSelectedIngredient] =
+    useState<Ingredient | null>(null);
+  const [grams, setGrams] = useState<number>(100);
+  const [currentCarbAmount, setCurrentCarbAmount] = useState<number>(100);
+
+  // To ensure the component only renders after it has mounted on the client
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  // Automatically update carb calculation and total carbs when grams input changes
+  useEffect(() => {
+    if (selectedIngredient && grams > 0) {
+      const carbAmount = (selectedIngredient.carbsPer100g * grams) / 100;
+      setCurrentCarbAmount(carbAmount);
+
+      // Calculate total carbs including the current input before adding it to the list
+      const total =
+        selectedIngredients.reduce(
+          (sum, ingredient) => sum + (ingredient.carbAmount || 0),
+          0
+        ) + carbAmount;
+      setTotalCarbs(total);
+    } else {
+      setCurrentCarbAmount(0);
+    }
+  }, [grams, selectedIngredient, selectedIngredients]);
+
+  const handleAddIngredient = () => {
+    if (selectedIngredient && grams > 0) {
+      const newIngredient = {
+        ...selectedIngredient,
+        grams,
+        carbAmount: currentCarbAmount,
+      };
+
+      setSelectedIngredients([...selectedIngredients, newIngredient]);
+
+      // Clear the input fields after adding the ingredient
+      setSelectedIngredient(null);
+      setGrams(0);
+      setCurrentCarbAmount(0);
+    }
+  };
+
+  const handleDeleteIngredient = (index: number) => {
+    const ingredientToRemove = selectedIngredients[index];
+    const updatedTotal = totalCarbs - (ingredientToRemove.carbAmount || 0);
+    setTotalCarbs(updatedTotal);
+    setSelectedIngredients(selectedIngredients.filter((_, i) => i !== index));
+  };
+
+  const handleClearAll = () => {
+    setSelectedIngredients([]);
+    setTotalCarbs(0);
+  };
+
+  if (!hasMounted) {
+    return null; // Avoid rendering the component server-side
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Kalkulator Ugljenih hidrata</h1>
+      <div>
+        <label htmlFor="ingredient" className="block mb-2">
+          Izaberi namirnicu
+        </label>
+        <select
+          id="ingredient"
+          className="text-black border p-2 mb-4"
+          value={selectedIngredient?.name || ""}
+          onChange={(e) => {
+            const selected = ingredientsList.find(
+              (ingredient) => ingredient.name === e.target.value
+            );
+            setSelectedIngredient(selected || null);
+          }}
+        >
+          <option value="">-- Izaberi namirnicu --</option>
+          {ingredientsList
+            .sort((a, b) => a.name.localeCompare(b.name)) // Sort the list alphabetically
+            .map((ingredient) => (
+              <option key={ingredient.name} value={ingredient.name}>
+                {ingredient.name}
+              </option>
+            ))}
+        </select>
+
+        {selectedIngredient && (
+          <>
+            <label htmlFor="grams" className="block mb-2">
+              Unesi grame
+            </label>
+            <input
+              type="number"
+              id="grams"
+              className="text-black border p-2 mb-4"
+              value={grams}
+              onChange={(e) => setGrams(Number(e.target.value))}
+            />
+            <p>
+              Ugljenih hidtrata u {grams}g {selectedIngredient.name}:{" "}
+              {currentCarbAmount.toFixed(2)}g
+            </p>
+          </>
+        )}
+
+        <button
+          className="bg-blue-500 text-white p-2 mt-2"
+
+          onClick={handleAddIngredient}
+        >
+          Dodaj namirnicu
+        </button>
+      </div>
+
+      <ul className="mt-4">
+        {selectedIngredients.map((ingredient, index) => (
+          <li key={index} className="mb-2">
+            {ingredient.name} - {ingredient.grams}g (
+            {ingredient.carbAmount?.toFixed(2)}g ugljenih hidrata)
+            <button
+              className="bg-red-500 text-white ml-4 p-1"
+              onClick={() => handleDeleteIngredient(index)}
+            >
+              Obrisi
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div className="mt-4">
+        <strong>Ukupno ugljenih hidrata: {totalCarbs.toFixed(2)}g</strong>
+      </div>
+
+      <button
+        className="bg-gray-500 text-white mt-4 p-2"
+        onClick={handleClearAll}
+      >
+        Obrisi sve
+      </button>
+    </div>
+  );
+};
+
+export default Home;
