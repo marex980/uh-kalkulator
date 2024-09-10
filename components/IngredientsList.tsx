909d09b875e4da25b9bd6bff47d98f5137d7c500
marex980 <69250892+marex980@users.noreply.github.com>
2024-09-10 13:44:25 +0200

    унете намирнице

--- components/IngredientsList.tsx
+++ components/IngredientsList.tsx
@@ -4,11 +4,19 @@ export interface Ingredient {
   }
   
   const ingredientsList: Ingredient[] = [
-    { name: "Јабука", carbsPer100g: 14 },
-    { name: "Банана", carbsPer100g: 23 },
-    { name: "Пиринач", carbsPer100g: 28 },
-    { name: "Пилетина", carbsPer100g: 0 },
-    { name: "Салата", carbsPer100g: 20 },
+    { name: "Чорба - парадајз без резанаца", carbsPer100g: 4 },
+    { name: "Резанци са сиром и јајима", carbsPer100g: 28 },
+    { name: "Супа Агро без резанаца", carbsPer100g: 0 },
+    { name: "Месо - батак/карабатак", carbsPer100g: 0 },
+    { name: "Сатараш", carbsPer100g: 7 },
+    { name: "Хлеб - парче", carbsPer100g: 15 },
+    { name: "Купус Сладак са јунетином", carbsPer100g: 6 },
+    { name: "Чорба - крем", carbsPer100g: 5 },
+    { name: "Месо - печење", carbsPer100g: 0 },
+    { name: "Поврће мешано", carbsPer100g: 7 },
+    { name: "Салата", carbsPer100g: 5 },
+    { name: "Ћуфте у парадајз сосу", carbsPer100g: 8 },
+    { name: "Колач", carbsPer100g: 50 },
   ];
   
   export default ingredientsList;
